71d799638d1b89abbafa947817363ba98a930766
mbotsko7 <mbotsko7@gmail.com>
2017-10-20 22:54:05 -0700

    initial parsing, beginning testing

--- chat_bot.js
+++ chat_bot.js
@@ -1,5 +1,6 @@
 var tmi = require('tmi.js');
 var fs = require('fs');
+var msg_count = 0; //the number of messages sent during the stream
 
 var options = {
     options: {
@@ -11,7 +12,7 @@ var options = {
     },
     identity: {
         username: "lenny_face_bot",
-        password: //PUT KEY HERE` 
+        password: 'oauth:na2hyb9rhoh6xhxon8wd8sr9sr2ys9'
     },
     channels: ["cmonte905"]
 };
@@ -23,5 +24,41 @@ client.connect();
 client.on('chat', function(channel, user, message, self) {
     var user_string = user["username"];
     var message_return = user_string + " " + message + "\n";
+    parse(user, msg)
     fs.appendFileSync('message.txt', message_return);
 });
+
+function getWordDictionary(){
+  //TODO: get the dictionary of word usage which should be contained in the stream log level of the JSON
+}
+
+function getEmojiDictionary(){
+  //TODO: dictionary of emojis
+}
+
+function parse(user, msg){
+    msg_count++;
+    var msg_arr = msg.split(" "); //split the message into array of string by whitespace
+    word_count = msg_arr.length; //get the count, this will be summed to the user's chattiness
+    caps_count = 0;              //count of the number of CAPITALIZED words in the message, summed for user
+    emoji_count = 0;             //count the number of emojis used, summed for user
+    for(word in msg_arr){
+      var dict = getWordDictionary(); //access word frequency dict for this session or streamer? unclear at this point
+      var emojiDict = getEmojiDictionary();
+      if(emojiDict[word] != undefined){
+        emoji_count++;
+      }
+      if(dict[word] != undefined){
+        dict[word]++;
+      }
+      else {
+        dict[word] = 1;
+      }
+      if(word === word.toUppercase()){
+        caps_count++;
+      }
+    }
+    Console.log(msg_count+" "+word_count+" "+caps_count+" "+emojiDict);
+
+
+}
