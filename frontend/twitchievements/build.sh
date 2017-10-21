3a5964f26299e67b64eec0491a03ecba149b5f7c
Aaron Turner <aaron@aaronthedev.com>
2017-10-21 11:55:55 -0700

    Added a quick file for building angular

---
+++ frontend/twitchievements/build.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# Following these for best builds
+# https://stackoverflow.com/questions/37631098/how-to-bundle-an-angular-app-for-production
+# https://github.com/angular/angular-cli/issues/3189
+
+ng build --prod --aot --no-sourcemap
